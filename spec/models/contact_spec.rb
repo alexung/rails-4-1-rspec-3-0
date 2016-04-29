8a2584cf69ad7984c532d6e584b2b57c85cc34ae
Alex Ung <alexung.ucsb@gmail.com>
2016-04-29 15:40:08 -0700

    init

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Contact do
+  it "is valid with a firstname, lastname and email" do
+    contact = Contact.new(firstname: 'Alex',
+      lastname: 'Ung',
+      email: 'tester@example.com')
+    expect(contact).to be_valid
+  end
+  it "is invalid without a firstname"
+  it "is invalid without a lastname"
+  it "is invalid without an email address"
+  it "is invalid with a duplicate email address"
+  it "returns a contact's full name as a string"
+end
