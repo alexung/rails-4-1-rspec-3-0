36e26b58c926e3745f2f86ca6774323fe25feaff
Alex Ung <alexung.ucsb@gmail.com>
2016-05-10 22:58:31 -0700

    phone specs with factory girl

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Contact do
-  
+
   it "is valid with a firstname, lastname and email" do
     contact = Contact.new(firstname: 'Alex',
       lastname: 'Ung',
@@ -10,40 +10,43 @@ describe Contact do
   end
 
   it "is invalid without a firstname" do
-    contact = Contact.new(firstname: nil)
+    contact = FactoryGirl.build(:contact, firstname: nil)
     contact.valid?
     expect(contact.errors[:firstname]).to include("can't be blank")
   end
-  
+
   it "is invalid without a lastname" do
-    contact = Contact.new(lastname: nil)
+    contact = FactoryGirl.build(:contact, lastname: nil)
     contact.valid?
     expect(contact.errors[:lastname]).to include("can't be blank")
   end
-  
+
   it "is invalid without an email address" do
-    contact = Contact.new(
+    contact = FactoryGirl.build(:contact,
       email: nil)
     contact.valid?
     expect(contact.errors[:email]).to include("can't be blank")
   end
-  
+
   it "is invalid with a duplicate email address" do
-    Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'tester@example.com')
-    contact = Contact.new(
-      firstname: 'Jane',
-      lastname: 'Tester',
-      email: 'tester@example.com')
+    FactoryGirl.create(
+      :contact,
+      email: 'alexung@gmail.com')
+    contact = FactoryGirl.build(
+      :contact,
+      email: 'alexung@gmail.com')
     contact.valid?
     expect(contact.errors[:email]).to include("has already been taken")
   end
-  
+
   it "returns a contact's full name as a string" do
-    contact = Contact.new(firstname: 'John', lastname: 'Doe', email: 'johndoe@example.com')
-    expect(contact.name).to eq 'John Doe'
+    contact = FactoryGirl.build(:contact, firstname: 'Jane', lastname: 'Smith')
+
+    expect(contact.name).to eq 'Jane Smith'
+  end
+
+  it "has three phone numbers" do
+    expect(create(:contact).phones.count).to eq 3
   end
 
   describe "filter last name by letter" do
@@ -66,4 +69,5 @@ describe Contact do
       end
     end
   end
+
 end
